a76744a20fd19ee3fbea5b836d08cf7d5632cf96
EladB1 <eladbergrin@gmail.com>
2024-05-29 13:02:38 -0400

    Started to work on algorithm for IR generation

--- src/main/java/com/piedpiper/bolt/ir/IRGenerator.java
+++ src/main/java/com/piedpiper/bolt/ir/IRGenerator.java
@@ -34,10 +34,18 @@ public class IRGenerator {
         else if (AST.matchesLabel("VAR-DECL")) {
             IR.addAll(generateVarDecl(AST.getChildren()));
         }
-        else if (AST.matchesValue("+"))
-            IR.addAll(generateAddition(AST.getChildren()));
-        else if (AST.matchesValue("*"))
-            IR.addAll(generateMultiplication(AST.getChildren()));
+        else if (AST.matchesValue("+") || AST.matchesValue("-") || AST.matchesValue("^") || AST.matchesValue("&"))
+            IR.addAll(generateAddition(AST));
+        else if (AST.matchesValue("*") || AST.matchesValue("/") || AST.matchesValue("%") || AST.matchesValue("**"))
+            IR.addAll(generateMultiplication(AST));
+        else if (AST.matchesValue("==") || AST.matchesValue("!=") || AST.matchesValue("<") || AST.matchesValue("<=") || AST.matchesValue(">") || AST.matchesValue(">="))
+            IR.addAll(generateComparison(AST));
+        else if (AST.matchesValue("||") || AST.matchesValue("&&"))
+            IR.addAll(generateLogicalExpression(AST));
+        else if (AST.matchesLabel("UNARY-OP"))
+            IR.addAll(generateUnaryOp(AST));
+        else if (AST.matchesLabel("FUNC-CALL"))
+            IR.addAll(generateFunctionCall(AST));
         else
             IR.add(generateConstant(AST));
         return IR;
@@ -58,22 +66,155 @@ public class IRGenerator {
     }
 
     private String generateConstant(AbstractSyntaxTree node) {
+        if (node.isStringLiteral())
+            return "\"" + node.getValue() + "\"";
         return node.getValue();
     }
 
-    private List<String> generateAddition(List<AbstractSyntaxTree> children) {
+    private List<String> generateAddition(AbstractSyntaxTree operation) {
+        List<AbstractSyntaxTree> children = operation.getChildren();
         List<String> IR = new ArrayList<>();
         IR.addAll(generate(children.get(1)));
         IR.addAll(generate(children.get(0)));
-        IR.add("ADD");
+        if (operation.matchesValue("+"))
+            IR.add("ADD");
+        if (operation.matchesValue("-"))
+            IR.add("SUB");
+        if (operation.matchesValue("^"))
+            IR.add("XOR");
+        if (operation.matchesValue("&"))
+            IR.add("B_AND");
         return IR;
     }
 
-    private List<String> generateMultiplication(List<AbstractSyntaxTree> children) {
+    private List<String> generateMultiplication(AbstractSyntaxTree operation) {
+        List<AbstractSyntaxTree> children = operation.getChildren();
         List<String> IR = new ArrayList<>();
         IR.addAll(generate(children.get(1)));
         IR.addAll(generate(children.get(0)));
-        IR.add("MUL");
+        if (operation.matchesValue("*"))
+            IR.add("MUL");
+        if (operation.matchesValue("/"))
+            IR.add("DIV");
+        if (operation.matchesValue("%"))
+            IR.add("REM");
+        if (operation.matchesValue("**"))
+            IR.add("POW");
+        return IR;
+    }
+
+    private List<String> generateUnaryOp(AbstractSyntaxTree node) {
+        AbstractSyntaxTree left = node.getChildren().get(0);
+        AbstractSyntaxTree right = node.getChildren().get(1);
+        List<String> IR = new ArrayList<>();
+        if (left.matchesValue("!")) {
+            IR.addAll(generate(right));
+            IR.add("NOT");
+        }
+        if (left.matchesValue("++")) {
+            IR.add("1");
+            IR.addAll(generate(right));
+            IR.add("ADD");
+        }
+        if (right.matchesValue("++")) {
+            IR.add("1");
+            IR.addAll(generate(left));
+            IR.add("ADD");
+        }
+        if (left.matchesValue("--")) {
+            IR.add("1");
+            IR.addAll(generate(right));
+            IR.add("SUB");
+        }
+        // TODO: Differentiate between increment/decrement operator on either side of variable
+        if (right.matchesValue("--")) {
+            IR.add("1");
+            IR.addAll(generate(left));
+            IR.add("SUB");
+        }
+        if (left.matchesValue("-")) {
+            IR.addAll(generate(right));
+            IR.add("0");
+            IR.add("SUB");
+        }
+        return IR;
+    }
+
+    private List<String> generateComparison(AbstractSyntaxTree comparison) {
+        List<AbstractSyntaxTree> children = comparison.getChildren();
+        List<String> IR = new ArrayList<>();
+        IR.addAll(generate(children.get(1)));
+        IR.addAll(generate(children.get(0)));
+        if (comparison.matchesValue("=="))
+            IR.add("EQ");
+        if (comparison.matchesValue("!="))
+            IR.add("NE");
+        if (comparison.matchesValue("<"))
+            IR.add("LT");
+        if (comparison.matchesValue("<="))
+            IR.add("LE");
+        if (comparison.matchesValue(">"))
+            IR.add("GT");
+        if (comparison.matchesValue(">="))
+            IR.add("GE");
+        return IR;
+    }
+
+    private List<String> generateLogicalExpression(AbstractSyntaxTree operator) {
+        List<String> IR = new ArrayList<>();
+        IR.addAll(generate(operator.getChildren().get(1)));
+        IR.addAll(generate(operator.getChildren().get(0)));
+        if (operator.matchesValue("||"))
+            IR.add("OR");
+        if (operator.matchesValue("&&"))
+            IR.add("AND");
+        return IR;
+    }
+
+    private List<String> generateTernary(AbstractSyntaxTree node) {
+        List<String> IR = new ArrayList<>();
+
+        return IR;
+    }
+
+    private List<String> generateArrayLiteral(AbstractSyntaxTree node) {
+        List<String> IR = new ArrayList<>();
+
+        return IR;
+    }
+
+    private List<String> generateArrayIndex(AbstractSyntaxTree node) {
+        List<String> IR = new ArrayList<>();
+
+        return IR;
+    }
+
+    // TODO: Match builtins to their IR names
+    private List<String> generateFunctionCall(AbstractSyntaxTree node) {
+        List<String> IR = new ArrayList<>();
+        IR.add("CALL");
+        IR.add(node.getChildren().get(0).getValue());
+        if (node.countChildren() == 1)
+            IR.add("0");
+        else {
+            List<AbstractSyntaxTree> params = node.getChildren().get(1).getChildren();
+            IR.add(String.valueOf(params.size()));
+            StringBuilder sb = new StringBuilder();
+            List<String> paramIR;
+            sb.append("(");
+            for (int i = 0; i < params.size(); i++) {
+                paramIR = generate(params.get(i));
+                for (int j = 0; j < paramIR.size(); j++) {
+                    sb.append(paramIR.get(j));
+                    if (j < paramIR.size() - 1)
+                        sb.append(" ");
+                }
+                if (i < params.size() - 1)
+                    sb.append(",");
+            }
+            sb.append(")");
+            IR.add(sb.toString());
+        }
         return IR;
     }
 }
--- src/test/java/com/piedpiper/bolt/ir/TestIRGenerator.java
+++ src/test/java/com/piedpiper/bolt/ir/TestIRGenerator.java
@@ -78,4 +78,81 @@ public class TestIRGenerator {
         assertEquals("MUL", result.get(8));
     }
 
+    @Test
+    void test_generate_multi_comparison() {
+        // Source code: 2 < 5 && 2 != 3
+        VariableToken two = new VariableToken(TokenType.NUMBER, "2");
+        AbstractSyntaxTree AST = new AbstractSyntaxTree("PROGRAM", List.of(
+            new AbstractSyntaxTree(new VariableToken(TokenType.OP, "&&"), List.of(
+                new AbstractSyntaxTree(new VariableToken(TokenType.OP, "<"), two, new VariableToken(TokenType.NUMBER, "5")),
+                new AbstractSyntaxTree(new VariableToken(TokenType.OP, "!="), two, new VariableToken(TokenType.NUMBER, "3"))
+            ))
+        ));
+
+        List<String> result = generator.generate(AST);
+        System.out.println(result.toString());
+        assertEquals(7, result.size());
+        assertEquals("3", result.get(0));
+        assertEquals("2", result.get(1));
+        assertEquals("NE", result.get(2));
+        assertEquals("5", result.get(3));
+        assertEquals("2", result.get(4));
+        assertEquals("LT", result.get(5));
+        assertEquals("AND", result.get(6));
+    }
+
+    @Test
+    void test_generateFunctionCall_builtin_noParams() {
+        // Source code: getEnv();
+        AbstractSyntaxTree AST = new AbstractSyntaxTree("PROGRAM", List.of(
+            new AbstractSyntaxTree("FUNC-CALL", new VariableToken(TokenType.ID, "getEnv"))
+        ));
+
+        List<String> result = generator.generate(AST);
+
+        assertEquals(3, result.size());
+        assertEquals("CALL", result.get(0));
+        assertEquals("getEnv", result.get(1));
+        assertEquals("0", result.get(2));
+    }
+
+    @Test
+    void test_generateFunctionCall_builtin_oneParam() {
+        // Source code: println(1+6);
+        AbstractSyntaxTree AST = new AbstractSyntaxTree("PROGRAM", List.of(
+            new AbstractSyntaxTree("FUNC-CALL", List.of(
+                new AbstractSyntaxTree(new VariableToken(TokenType.ID, "println")),
+                new AbstractSyntaxTree("FUNC-PARAMS", List.of(
+                    new AbstractSyntaxTree(new VariableToken(TokenType.OP, "+"), new VariableToken(TokenType.NUMBER, "1"), new VariableToken(TokenType.NUMBER, "6"))
+                ))
+            ))
+        ));
+
+        List<String> result = generator.generate(AST);
+
+        assertEquals(4, result.size());
+        assertEquals("CALL", result.get(0));
+        assertEquals("println", result.get(1));
+        assertEquals("1", result.get(2));
+        assertEquals("(6 1 ADD)", result.get(3));
+    }
+
+    @Test
+    void test_generateFunctionCall_builtin_multiParam() {
+        // Source code: slice("hello, world!", 1, 6);
+        AbstractSyntaxTree AST = new AbstractSyntaxTree("PROGRAM", List.of(
+            new AbstractSyntaxTree("FUNC-CALL", List.of(
+                new AbstractSyntaxTree(new VariableToken(TokenType.ID, "slice")),
+                new AbstractSyntaxTree("FUNC-PARAMS", new VariableToken(TokenType.STRING, "hello, world!"), new VariableToken(TokenType.NUMBER, "1"), new VariableToken(TokenType.NUMBER, "6"))
+            ))
+        ));
+
+        List<String> result = generator.generate(AST);
+
+        assertEquals(4, result.size());
+        assertEquals("CALL", result.get(0));
+        assertEquals("slice", result.get(1));
+        assertEquals("3", result.get(2));
+        assertEquals("(\"hello, world!\",1,6)", result.get(3));
+    }
 }
